3c787af90e657423d2659dcb33d858db07591a4e
tom whitehead <twhitehead.1991@gmail.com>
2026-04-03 22:20:41 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ echo "--- 🔐 Logging into ECR ---"
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO_URL
 
 echo "--- 🏗️ Building and Tagging ---"
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME:latest $REPO_URL:latest
+sudo docker build -t $IMAGE_NAME .
+sudo docker tag $IMAGE_NAME:latest $REPO_URL:latest
 
 echo "--- 🚀 Pushing to ECR ---"
-docker push $REPO_URL:latest
+sudo docker push $REPO_URL:latest
 
 echo "--- 🔄 Refreshing ECS Service ---"
-aws ecs express deploy --name $SERVICE_NAME --image $REPO_URL:latest --update
+aws ecs update-service --cluster default --service $SERVICE_NAME --force-new-deployment
 
 echo "--- ✅ Deployment Started! ---"
